cac906d0025397dfb39e48d86bb4003ad5e7d672
SamyuktaNair <162045674+SamyuktaNair@users.noreply.github.com>
2024-11-12 09:36:14 +0530

    jobsearch navigation

--- frontend/job_portal/src/Pages/Home.jsx
+++ frontend/job_portal/src/Pages/Home.jsx
@@ -4,9 +4,16 @@ import Footer from '../components/Footer'
 import './home.css'
 import { useState,useEffect } from 'react'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 
 const Home = () => {
+    const navigate=useNavigate()
+    const handleClick = () => {
+        navigate('/jobsearch');
+      };
+
+
     const [latestJobs, setLatestjobs]=useState([]);
 
     useEffect(()=>{
@@ -33,10 +40,10 @@ const Home = () => {
                 <p>
                     Find Your Dream Job Today !!
                 </p>
-                <input type="text"  placeholder='Search for jobs '/>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <input type="text"  placeholder='Search for jobs ' onClick={handleClick}/>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                </svg>
+                </svg> */}
 
 
             </div>
